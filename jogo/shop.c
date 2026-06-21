3bce7af7d0ed4c17d477ae7c6b8b9cb207ba1de8
metira837 <tomiokatomiokaoff@gmail.com>
2026-06-21 02:59:19 -0300

    making the code in shop.c better for read

--- jogo/shop.c
+++ jogo/shop.c
@@ -2,14 +2,38 @@
 #include "shop.h"
 #include <string.h>
 
-static void option(short int options, short int money, struct perso *perso1){
-	short int a; 
-	struct attack skills[4] = {{20, "wind", 20, "ball of wind"}, {20, "fire", 20, "ball of fire"}, {20, "water", 20, "ball of water"}, {20,"earth", 20, "ball of earth"}};
-       
 
-	switch(options)	{
+
+short int show_shop(short int options){
+	short int a;
+	switch(options){
 		case 1:
 			printf("1-ball of wind(cost 30 money)\n2-ball of fire(cost 30 money)\n3-ball of water(cost 30 money)\n4-ball of earth(cost 30 money)\n"); scanf("%hd", &a);
+			break;
+		case 2:
+			printf("1-healing Potion(cost 50 money)\n"); scanf("%hd", &a);
+			break;
+		case 3:
+			printf("leaving of shop...\n");
+			break;
+		default:
+			printf("this option not exist lol\n");
+
+			break;
+	}
+	return a;
+}
+
+
+static void option(short int money, struct perso *perso1){
+	short int a, options; 
+	struct attack skills[4] = {{20, "wind", 20, "ball of wind"}, {20, "fire", 20, "ball of fire"}, {20, "water", 20, "ball of water"}, {20,"earth", 20, "ball of earth"}};
+       	option:
+		printf("select option that wants buy:\n1-moves 2-items\n3-quit of shop\n"); scanf("%hd", &options);
+	
+	switch(options)	{
+		case 1:
+			a = show_shop(options);
 			if(money < 30)
 				printf("necessary 30 of money");
 			else{
@@ -21,7 +45,7 @@ static void option(short int options, short int money, struct perso *perso1){
 			}
 			break;
 		case 2:
-			printf("1-healing Potion(cost 50 money)\n");
+			a = show_shop(options);
 			if(money < 50)
 				printf("necessary 50 of money");
 			else{
@@ -29,18 +53,19 @@ static void option(short int options, short int money, struct perso *perso1){
 				
 			}
 			break;
-		default:
-			printf("this option not exist lol");
+		case 3:
+			a = show_shop(options);
 			break;
+		default:
+			a = show_shop(options);
+			goto option;
 	}
 	
 	
 }
 void shop(short int money, struct perso *perso1){
-	short int options = 0;
 	printf("welcome the shop\nyou has %d of money\n", money);
-	printf("select option that wants buy:\n1-moves 2-items\n"); scanf("%hd", &options);
-	option(options, money, perso1);	
+	option(money, perso1);	
 
 }
 
