00a50b52c9d7cbf937fa37839ab99893c58d7b4e
metira837 <tomiokatomiokaoff@gmail.com>
2026-06-21 17:52:58 -0300

    check_bag and check_attack new functions in shop.c

--- jogo/shop.c
+++ jogo/shop.c
@@ -3,26 +3,29 @@
 #include <string.h>
 #include <stdbool.h>
 
+static short int check_bag(struct perso *perso1){
+	short int b = 0;
+	for(;b > 4; b++){
+		if(perso1->attacks[b].pp == 0){
+			break;
+		}
+		
+	}
+	printf("b = %d\n",b);
+	return b; 	
+	
+}
 
-static bool check(char name[],struct perso *perso1, short int options){
+static bool check_attack(char name[],struct perso *perso1){
 	short int a = 3;
 	bool ks = false;
-	if(options == 1){
-		for(; a > 0; a--){
-			if((strcmp(name, perso1->attacks[a].name)) == 0){
-				ks = true;
-				break;
-			}
-				
-		}
-	}
-
-	else{
-		printf("binga");
-	}
-	return ks;		
-	
-	
+	for(; a > 0; a--){
+		if((strcmp(name, perso1->attacks[a].name)) == 0){
+			ks = true;
+			break;
+		}			
+	}	
+	return ks;			
 }
 
 
@@ -33,7 +36,7 @@ static short int show_shop(short int options){
 			printf("1-ball of wind(cost 30 money)\n2-ball of fire(cost 30 money)\n3-ball of water(cost 30 money)\n4-ball of earth(cost 30 money)\n"); scanf("%hd", &a);
 			break;
 		case 2:
-			printf("1-healing Potion(cost 50 money)\n2-poison potion(cost 50 of money)"); scanf("%hd", &a);
+			printf("1-heal Potion(cost 50 money)\n2-poison potion(cost 50 of money)\n"); scanf("%hd", &a);
 			break;
 		case 3:
 			printf("leaving of shop...\n");
@@ -67,7 +70,7 @@ static void option(short int money, struct perso *perso1){
 			else if(money < 30)
 				printf("necessary 30 of money");
 
-			else if(check(skills[a-1].name, perso1, options)){
+			else if(check_attack(skills[a-1].name, perso1)){
 					printf("you has this skill\n");
 			       }
 					
@@ -83,17 +86,19 @@ static void option(short int money, struct perso *perso1){
 			break;
 		case 2:
 			a = show_shop(options);
-			if(a > 1){
+			if(a > 2){
 				printf("this option not exist\n");
 				goto option;
 			}
 			else if(money < 50)
 				printf("necessary 50 of money");
+			
 			else{
-				printf("you obtained the potion");
+				short int po = check_bag(perso1);
+				printf("you obtained the potion %s\n", itens1[a-1].name);
 				perso1->money -= 50;
-				strcpy(perso1->itens[1].name, itens1[a-1].name);
-				perso1->itens[1].quantity += itens1[a-1].quantity;
+				strcpy(perso1->itens[po].name, itens1[a-1].name);
+				perso1->itens[po].quantity += itens1[a-1].quantity;
 			}
 			break;
 		case 3:
