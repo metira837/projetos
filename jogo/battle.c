d5c8630b31b2918bd7e3d1377b49e31e9943aedf
metira837 <tomiokatomiokaoff@gmail.com>
2026-07-28 22:15:53 -0500

    solved a error of sintaxe

--- jogo/battle.c
+++ jogo/battle.c
@@ -3,11 +3,11 @@
 #include "perso.h"
 #include "enemy.h"
 
-short int  attack_perso(struct perso *perso1, struct enemy *enemy1, struct attack atk_perso){
+short int  attack_perso(struct perso *perso1, struct enemy *enemy1, struct attack *atk_perso){
 	short int damage1 = 0;
 	damage1 += perso1->damage;
 	if(atk_perso->pp == 0)
-			return 0;
+		return 0;
 
 	if(strcmp(perso1->type, atk_perso->type ) == 0 )
 		damage1 *= 1.5;	
@@ -16,7 +16,7 @@ short int  attack_perso(struct perso *perso1, struct enemy *enemy1, struct attac
 	}
 
 void attack_enemy(struct perso *perso1, struct enemy *enemy1,  struct attack_enemy atk_enemy){
-		short int damage = 0;
-		damage = (enemy1->damage + atk_enemy.damage) * 1.5;	
+	short int damage = 0;
+	damage = (enemy1->damage + atk_enemy.damage) * 1.5;	
 	perso1->hp -= damage;
 }
