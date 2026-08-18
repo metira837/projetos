1ee36f8c16690a15cfea967aad9939689c3a65c2
metira837 <tomiokatomiokaoff@gmail.com>
2026-08-18 16:05:19 -0500

    add a for to draw rectangle

--- game_with_raylib/game1/ks.c
+++ game_with_raylib/game1/ks.c
@@ -1,27 +1,31 @@
 #include "raylib.h"
-
+#include <stdlib.h>
 
 int main(void){
 	const int screenwidth = 1500, screenheight = 700;
+	short int jump = 0;
 	Vector2 ball = {700, (screenheight/2)-50};
-	Rectangle rec = {0, screenheight/2, 5000, 1000};
 	InitWindow(screenwidth, screenheight, "nada");
+	
+	Rectangle recs[] =  {{0, screenheight/2, 5000, 1000}, {0, ball.y - 400, 5000, 300}, {-500, -150, 500, 2000}};
+
 
 	Camera2D ca = {0};
-	ca.target = (Vector2) {ball.x + 20.0f, ball.y + 20.0f};
+	ca.target = ball;
 	ca.offset = (Vector2) {screenwidth/2, screenheight/2};
 	ca.rotation = 0.0f;
-	ca.zoom = 1.0f;
+	ca.zoom = 1.5f;
 
 	SetTargetFPS(60);
-
+	
 
 	while(!WindowShouldClose()){
+		float deltatime = GetFrameTime();
 		if(IsKeyDown(KEY_D)){
-			ball.x += 10;
+			ball.x += 10; 
 		}
-		if(IsKeyDown(KEY_W)){
-			ball.y -= 10;	
+		if(IsKeyDown(KEY_W) && ball.y == (screenheight / 2) - 20){
+			ball.y += -50;
 		}
 		if(IsKeyDown(KEY_S)){
 			ball.y += 10;
@@ -30,27 +34,25 @@ int main(void){
 			ball.x -= 10;
 		
 		}
-		if(CheckCollisionCircleRec(ball, 10.0f, rec)){
+		if(CheckCollisionCircleRec(ball, 10.0f, recs[0])){
 			ball.y = (screenheight/2) - 20;
 		}
 	
-		if(ball.x > rec.width || ball.x < 0 || ball.y != (screenheight/2) - 20){
-				ball.y += 5;
+		if(ball.x > recs[0].width || ball.x < 0 || ball.y != (screenheight/2) - 20){
+				ball.y += 1;		
 		
 		}
-
-		ca.target = (Vector2) {ball.x + 10, ball.y + 10};
+		ca.target = ball;
 			
 		BeginDrawing();
 		  ClearBackground(RAYWHITE);	
 		  BeginMode2D(ca);
-			DrawCircleV(ball, 20,  RED);
-			DrawRectangleRec(rec, DARKGRAY);
+		  	DrawCircleV(ball, 20,  RED);
 			DrawText("merda na chaleira", 500, 300, 50, DARKGRAY);
-			
+			for(int a = 0; a < sizeof(recs) / sizeof(recs[1]); a++) DrawRectangleRec(recs[a], DARKGRAY);
 
 		  EndMode2D();	
-		  
+
 		EndDrawing();
 	
 	
