e8dd9ae3c6fb781c1781a569d2c6a2ec57a05a83
metira837 <tomiokatomiokaoff@gmail.com>
2026-08-20 19:23:31 -0500

    add walls to fight

--- game_with_raylib/game1/ks.c
+++ game_with_raylib/game1/ks.c
@@ -11,7 +11,7 @@ int main(void){
 	InitWindow(screenwidth, screenheight, "nada");
 	
 	Rectangle recs[] =  {{0, screenheight/2, 5000, 1000}, {0, ball.y - 400, 3000, 300}, {0, -150, 50, 2000}};
-	Rectangle battle[] = {{3000, 600, 2000, 300}, {5000, -100, 100, 1000}};
+	Rectangle battle[] = {{2800, 0, 200, 1000}, {5000, -100, 100, 1000}};
 	bool start_battle = false;
 	Camera2D ca = {0};
 	ca.target = ball;
@@ -37,14 +37,15 @@ int main(void){
 		if(CheckCollisionCircleRec(ball, 10.0f, recs[0])) ball.y = screenheight/2 - 20;
 		if(CheckCollisionCircleRec(ball, 1.0f, recs[1])) ball.y += 100;
 		if(CheckCollisionCircleRec(ball, 10.0f, recs[2])) ball.x = 70;
-		if(CheckCollisionCircleRec(ball, 15.0f, battle[1])) ball.x -= 10;
-	
-		if(ball.x > recs[0].width || ball.x < 0 || ball.y != (screenheight/2) - 20) ball.y += 2;		
-		if(ball.x > recs[1].width) start_battle = true;
+		if(start_battle){
+			if(CheckCollisionCircleRec(ball, 15.0f, battle[1])) ball.x -= 10;
+			if(CheckCollisionCircleRec(ball, 15.0f, battle[0])) ball.x += 10; 	
 		
-
-
-
+		}
+		
+		if(ball.x > recs[0].width || ball.x < 0 || ball.y != (screenheight/2) - 20) ball.y += 2;		
+		if(ball.x > recs[1].width) start_battle = true;	
+		
 		ca.target = Vector2Lerp(ca.target, ball, 0.100f);
 	
 		BeginDrawing();
@@ -53,8 +54,11 @@ int main(void){
 		  	DrawCircleV(ball, 20,  RED);
 			DrawText("merda na chaleira", 500, 300, 50, DARKGRAY);
 			draw_rectangle(recs, sizeof(recs) /  sizeof(recs[0]));		
-			draw_rectangle(battle, sizeof(battle) /  sizeof(battle[0]));		
-
+			if(start_battle){
+				draw_rectangle(battle, sizeof(battle) /  sizeof(battle[0]));		
+		
+			
+			}
 
 		 EndMode2D();	
 		EndDrawing();
