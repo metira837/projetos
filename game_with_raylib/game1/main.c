0ddcd68a1c91d464642a4127b7c78d6f8367abb7
metira837 <tomiokatomiokaoff@gmail.com>
2026-08-27 17:14:14 -0500

    shoot more speed

--- game_with_raylib/game1/main.c
+++ game_with_raylib/game1/main.c
@@ -88,12 +88,12 @@ int main(void){
 		
 		if(perso1.can_attack == false){
 			if(perso1.side == true){
-				perso1.attack.x -= 5;	
+				perso1.attack.x -= 10;	
 				if(collision_attack(recs, sizeof(recs) / sizeof(recs[0]), perso1.attack)) perso1.can_attack = true;
 				if(collision_attack(battle, sizeof(battle) / sizeof(battle[0]), perso1.attack)) perso1.can_attack = true;
 			}	
 			if(perso1.side == false){
-				perso1.attack.x += 5;	
+				perso1.attack.x += 10;	
 				if(collision_attack(recs, sizeof(recs) / sizeof(recs[0]), perso1.attack)) perso1.can_attack = true;
 				if(collision_attack(battle, sizeof(battle) / sizeof(battle[0]), perso1.attack)) perso1.can_attack = true;
 			}
